2f7aeec52ab8cd9db6eebced112f48370311d417
mallobcd <m2bc2d.pro@gmail.com>
2026-03-12 15:46:46 +0100

    Refactor waitForAnalysisCompletion to accept metrics slice for dynamic API calls

--- metric/sonarQubeAnalyzer.go
+++ metric/sonarQubeAnalyzer.go
@@ -60,7 +60,7 @@ func (a *SonarQubeAnalyzer) AnalyzeProject(repoName string, path string, metrics
 			return err
 		}
 
-		if err := waitForAnalysisCompletion(projectName, len(metrics), 5*time.Minute); err != nil {
+		if err := waitForAnalysisCompletion(projectName, metrics, 5*time.Minute); err != nil {
 			return err
 		}
 
@@ -79,7 +79,7 @@ func (a *SonarQubeAnalyzer) AnalyzeProject(repoName string, path string, metrics
 	return nil
 }
 
-func waitForAnalysisCompletion(projectName string, numberOfMetrics int, timeout time.Duration) error {
+func waitForAnalysisCompletion(projectName string, metrics []string, timeout time.Duration) error {
 	sonarURL := os.Getenv("SONAR_URL")
 	sonarToken := os.Getenv("SONAR_TOKEN")
 	client := helper.NewHTTPClient(sonarURL, sonarToken)
@@ -91,12 +91,12 @@ func waitForAnalysisCompletion(projectName string, numberOfMetrics int, timeout 
 	for {
 		select {
 		case <-ticker.C:
-			path := fmt.Sprintf("/api/measures/component?metricKeys=ncloc&component=%s", projectName)
+			path := fmt.Sprintf("/api/measures/component?metricKeys=%s&component=%s", strings.Join(metrics, ","), projectName)
 
 			var resp model.SonarMeasures
 			err := client.DoRequest("GET", path, nil, &resp)
 
-			if err == nil && len(resp.Component.Measures) != numberOfMetrics {
+			if err == nil && len(resp.Component.Measures) == len(metrics) {
 				return nil
 			}
 
